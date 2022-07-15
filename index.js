15facc07f5f824c56798f507f0b5e8e5e32e6345
MR.Knorth <46872981+ChenhaoZhu@users.noreply.github.com>
2022-07-15 08:33:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 var currentIndex = 1;
 var totalCount = 3;
-const mus = ['images/background-girlwithsky.jpeg', 'images/background-girlattree.jpeg', 'images/background-boyonmask.jpeg', 'images/background-girlwithdesk.jpeg', 'images/blackground-girl.jpeg']
+const mus = ['images/background-girlattree.jpeg', 'images/background-boyonmask.jpeg', 'images/background-girlwithdesk.jpeg', 'images/blackground-girl.jpeg']
 let topp = document.getElementsByClassName('top-container')
 var curImgId = 0;
 setInterval(function() {
 
     $(topp[0]).css('background-image','url("'+ mus[curImgId]);
 
-    curImgId = (curImgId + 1) % 5;
+    curImgId = (curImgId + 1) % 4;
     // console.log(topp[0].innerHTML);
 }, 4000);

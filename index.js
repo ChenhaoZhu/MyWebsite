51e1905e6d934d48b2ee6a5ce28caeced0b5b316
MR.Knorth <46872981+ChenhaoZhu@users.noreply.github.com>
2022-07-15 09:22:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,52 @@ setInterval(function() {
     curImgId = (curImgId + 1) % 9;
     // console.log(topp[0].innerHTML);
 }, 4000);
+
+
+var words = ['A Mysterious Man', 'An Interesting Programmer'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 15,
+    speed = 100;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    $('.firstword').text(part);
+  },speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
+

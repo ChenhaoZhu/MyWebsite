973ae7a35d89c563ec457d8538ea6f7cdcf35955
MR.Knorth <46872981+ChenhaoZhu@users.noreply.github.com>
2022-07-15 08:46:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 var currentIndex = 1;
 var totalCount = 3;
-const mus = ['images/background-girlwithsky.jpeg', 'images/background-girlattree.jpeg', 'images/background-boyonmask.jpeg', 'images/background-girlwithdesk.jpeg', 'images/blackground-girl.jpeg']
+const mus = ['images/background-girlwithsky.jpeg', 'images/background-girlattree.jpeg', 'images/background-boyonmask.jpeg', 'images/background-girlwithdesk.jpeg', 'images/blackground-girl.jpeg', 'images/background-boywithdog.jpeg', 'images/background-girlwithfeet.jpeg', 'images/background-girleat.jpeg', 'images/background-piano.jpeg']
 let topp = document.getElementsByClassName('top-container')
 var curImgId = 0;
 setInterval(function() {
 
     $(topp[0]).css('background-image','url("'+ mus[curImgId]);
 
-    curImgId = (curImgId + 1) % 5;
+    curImgId = (curImgId + 1) % 9;
     // console.log(topp[0].innerHTML);
 }, 4000);

7da9f794f5ef4409f1ed1cf5f49a18227cf806be
MR.Knorth <46872981+ChenhaoZhu@users.noreply.github.com>
2022-07-15 00:20:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 var currentIndex = 1;
 var totalCount = 3;
-const mus = ['images/background-boyonmask.jpeg', 'images/background-girlwithdesk.jpeg', 'images/blackground-girl.jpeg']
+const mus = ['images/beautifulSkyForDaytime.jpg', 'images/background-boyonmask.jpeg', 'images/background-girlwithdesk.jpeg', 'images/blackground-girl.jpeg']
 let topp = document.getElementsByClassName('top-container')
 var curImgId = 0;
 setInterval(function() {
 
     $(topp[0]).css('background-image','url("'+ mus[curImgId]);
 
-    curImgId = (curImgId + 1) % 3;
+    curImgId = (curImgId + 1) % 4;
     // console.log(topp[0].innerHTML);
 }, 5000);

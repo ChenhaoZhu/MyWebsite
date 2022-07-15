6eafb343511b495302fd5e9b820a47dfc67f13a3
MR.Knorth <46872981+ChenhaoZhu@users.noreply.github.com>
2022-07-15 14:46:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 
 var currentIndex = 1;
 var totalCount = 3;
-const mus = ['images/background-girlwithsky.jpeg', 'images/background-girlattree.jpeg', 'images/background-boyonmask.jpeg', 'images/background-girlwithdesk.jpeg', 'images/blackground-girl.jpeg', 'images/background-boywithdog.jpeg', 'images/background-girlwithfeet.jpeg', 'images/background-girleat.jpeg', 'images/background-piano.jpeg']
+const mus = 
+      ['images/background-girlwithsky.jpeg', 'images/background-girlattree.jpeg', 'images/background-boyonmask.jpeg', 
+       'images/background-girlwithdesk.jpeg', 'images/blackground-girl.jpeg', 'images/background-boywithdog.jpeg', 
+       'images/background-girlwithfeet.jpeg', 'images/background-girleat.jpeg', 'images/background-piano.jpeg',
+      'images/background-touxing.jpeg', 'images/background-girlandwindow.jpeg', 'images/background-jojo.jpeg']
+
 let topp = document.getElementsByClassName('top-container')
 var curImgId = 0;
 setInterval(function() {
 
     $(topp[0]).css('background-image','url("'+ mus[curImgId]);
 
-    curImgId = (curImgId + 1) % 9;
+    curImgId = (curImgId + 1) % 12;
     // console.log(topp[0].innerHTML);
 }, 4000);
 
